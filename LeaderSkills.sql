7a8dcde39d3568bc01aaeff752ddd96334f3de38
DESKTOP-BAI3V0A\boltf <ryanbreau2@gmail.com>
2017-04-19 11:59:17 -0300

    V.1

---
+++ LeaderSkills.sql
@@ -0,0 +1,33 @@
+/*	
+	Name:	 Ryan Breau
+	Date:	 04/19/17
+	Purpose: Inserts for the leader skills of the monsters added by the memebers of the class
+
+	V.1-
+*/
+
+--Ryan
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
+('Evil Sound''s Reverberation', '2.5x ATK with 3 Att.(3x for 4 Att.); 4x ATK & reduces dmg with a cross of 5 Heal Orbs'),
+('Light of Scathach', '3x ATK for Balanced Type'),
+('Hermit Soul', '2x ATK & RCV for Wood Att.; 3x ATK when Wood & Water attack at once'),
+('Hermit Eyes', 'Wood attribute cards ATK x2, RCV x2; all attribute cards ATK x3.5 when reaching Wood & Water combos'),
+('Ancient Dragon Tribe''s Quintessence','2.5x ATK & RCV for Dragon & Balanced Types; 1.5x ATK when erasing 4+ linked Wood Orbs'),
+('Prosperity God''s Cadence','2.5x ATK & 1.5x RCV for Wood Att.; 2x ATK with 2 Wood combos, 3x ATK with 3+ combos'),
+('Demonic Strike', '3x ATK when erasing 5 Orbs with 1+ enhanced; ATK increases with 6+ combos up to 2.5x');
+
+--Zach
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
+('Death Sea Holy Water','ATK x3 when simultaneously clearing 5 connected Water or Dark Orbs.');
+
+--Elie
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
+();
+
+--Kyle
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
+();
+
+--Billy
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
+();
