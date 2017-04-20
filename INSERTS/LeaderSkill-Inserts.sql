ff80b4f3ab4c4369fb21e891e2f2d7f2af281b5b
Ryan Breau <ryanbreau2@gmail.com>
2017-04-19 22:31:54 -0300

    Changed descriptions to use the in-game terminology

--- INSERTS/LeaderSkill-Inserts.sql
+++ INSERTS/LeaderSkill-Inserts.sql
@@ -9,54 +9,57 @@
 
 TRUNCaTE table SWTS1103.dbo.LeaderSkill
 
+--Common
+
+
 --Ryan
-insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDesc) values
 ('Evil Sound''s Reverberation', '2.5x ATK with 3 Att.(3x for 4 Att.); 4x ATK & reduces dmg with a cross of 5 Heal Orbs'),
 ('Light of Scathach', '3x ATK for Balanced Type'),
 ('Hermit Soul', '2x ATK & RCV for Wood Att.; 3x ATK when Wood & Water attack at once'),
-('Hermit Eyes', 'Wood attribute cards ATK x2, RCV x2; all attribute cards ATK x3.5 when reaching Wood & Water combos'),
+('Hermit Eyes', '2x ATK & RCV for Wood Att.; 3.5x ATK when Wood & Water attack at once'),
 ('Ancient Dragon Tribe''s Quintessence','2.5x ATK & RCV for Dragon & Balanced Types; 1.5x ATK when erasing 4+ linked Wood Orbs'),
 ('Prosperity God''s Cadence','2.5x ATK & 1.5x RCV for Wood Att.; 2x ATK with 2 Wood combos, 3x ATK with 3+ combos'),
 ('Demonic Strike', '3x ATK when erasing 5 Orbs with 1+ enhanced; ATK increases with 6+ combos up to 2.5x');
 
 --Zach
-insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
-('Death Sea Holy Water','ATK x3 when simultaneously clearing 5 connected Water or Dark Orbs. ATK x0.5 for each additional Orb, up to ATK x6 at 11 connected Orbs. God type cards RCV x1.5'),
-('I''ll bring this to an end!','Attacker type cards HP x1.5, RCV x1.5. All attribute cards ATK x2 on the turn a skill is used'),
-('Iron Wall Lore','20% all damage reduction'),
-('Demon''s Sacred Talisman','Dragon & Attacker type Cards ATK x3.5, RCV x1.5'),
-('Demon''s Dragon Talisman','Dragon & Attacker type cards ATK x3.5, RCV x1.5. All attribute cards ATK x2 when reaching 7 combos or above'),
-('Dark Steel Star of Libra','Machine type cards ATK x1.5, RCV x1.5. Matched attribute ATK x4 when matching exactly 5 connected orbs with at least 1 enhanced orb'),
-('Game of the Dead','Dark attribute cards HP x1.35, RCV x1.35. All attribute cards ATK x3 when attacking with 3 of the following orb types: Fire, Water, Wood, Light, Dark & Heart'),
-('Enjoy your holidays...','God & Machine type cards ATK x1.3, RCV x1.3. Matched attribute ATK x5 when matching exactly 5 connected orbs with at least 1 enhanced orb'),
-('Magic Dragon Tribe''s Determination','Dragon & Attacker type cards ATK x2.5, RCV x2.5. All attribute cards ATK x1.5 on the turn a skill is used');
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDesc) values
+('Death Sea Holy Water','ATK increases when erasing 5+ linked Water or Dark Orbs, up to 6x; 1.5x RCV for God Type'),
+('I''ll bring this to an end!','1.5x HP & RCV for Attacker Type; 2x ATK when skills used'),
+('Iron Wall Lore','Reduces damage'),
+('Demon''s Sacred Talisman','3.5x ATK & 1.5x RCV for Dragon & Attacker Types'),
+('Demon''s Dragon Talisman','3.5x ATK & 1.5x RCV for Dragon & Attacker Types; 2x ATK with 7+ combos'),
+('Dark Steel Star of Libra','1.5x ATK & RCV for Machine Type; 4x ATK for Att. erasing 5 Orbs including enhanced'),
+('Game of the Dead','Dark Att. HP & RCV increase a little; 3x ATK when attacking with 3+ Att. (2+Heal) at once'),
+('Enjoy your holidays...','ATK & RCV increase a little for God & Machine Types; 5x ATK for Att. erasing 5 Orbs including enhanced'),
+('Magic Dragon Tribe''s Determination','2.5x ATK & RCV for Dragon & Attacker Types; 1.5x ATK when skills used');
 
 --Elie
-insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
-('Dark Power','Dark attribute cards ATK x1.5'),
-('Power of Moondragon','Dark attribute cards ATK x2, RCV x2'),
-('Dance of the Crimson Heaven','All attribute cards ATK x4 when attacking with Fire, Wood & Light orb types at the same time'),
-('Grace of Water','Water attribute cards HP x2.5'),
-('Nirvana Soul','Wood attribute & God type cards HPx2, ATK x2'),
-('Dawn of a Dark Age','Attacker type cards HP x1.35, ATK x3'),
-('Dispelling Circle','Attacker type cards ATK x3. Heal RCV x5 as HP after every orbs elimination'),
-('Twin Sorcerous Curse - DK / Wd','Dark attribute cards ATK x2.5. Wood attribute cards ATK x1.4. ATK x3.5 if both attributes are met'),
-('Rumbling Firedragon''Bravery','Dragon type cards RCV x1.5. Fire attribute cards ATK x3'),
-('Nirvana Blade','Wood attribute & God type cards HP x2, ATK x2. All attribute cards ATK x2 when reaching Wood & Light combos');
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDesc) values
+('Dark Power','1.5x ATK for Dark Att.'),
+('Power of Moondragon','2x ATK & RCV for Dark Att.'),
+('Dance of the Crimson Heaven','4x ATK when Fire, Wood, & Light attack at once'),
+('Grace of Water','2.5x HP for Water Att.'),
+('Nirvana Soul','2x ATK & HP for Wood Att. and God Type'),
+('Dawn of a Dark Age','3x ATK for Attacker Type and their HP increases a little'),
+('Dispelling Circle','3x ATK for Attacker Type; heals HP equal to 5x RCV when matching Orbs'),
+('Twin Sorcerous Curse - DK / Wd','2.5x ATK for Dark, Wood Att. ATK increases a little; 3.5x ATK for allies with both Attributes'),
+('Rumbling Firedragon''s Bravery','1.5x RCV for Dragon Type; 3x ATK for Fire Att.'),
+('Nirvana Blade','2x HP & ATK for Wood Att. & God Type; 2x ATK when Wood & Light attack at once');
 
 --Kyle
-insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
-('Mechanical Dark Star of Gemini','Changes Jammers, Poison & Heal Orbs to Dark Orbs; other allies'' skills charged by 1 turn'),
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDesc) values
+('Mechanical Dark Star of Gemini','4x ATK for Att. erasing 5 Orbs with 1+ enhanced; reduces non-Dark Att. damage'),
 ('Rune of Fate','2.5x ATK for God Type'),
 ('Song of Valhalla','2.5x ATK & 1.5x HP for Fire Att. and God Type'),
 ('Dawn of Valhalla','2x HP & 2.5x ATK for Fire Att. & God Type; 1.5x ATK erasing 4+ linked Fire Orbs'),
 ('Axiom of Sustainment','ATK increases with 5+ combos, up to 6x ATK; bonus attack of 30x ATK when matching Orbs'),
 ('The Armiger','1.5x to all stats, but unable to erase 3 Orbs or less; ATK increases with 3+ combos, up to 7x'),
 ('Sight of Valhalla','2.5x ATK & RCV for Fire Att. & God Type; 2.5x ATK when erasing 6+ linked Fire Orbs'),
-('Soul of the Darkdragon','	2x ATK & HP for Dragon & Devil Types; 3.5x ATK when Light & Dark attack at once');
+('Soul of the Darkdragon','2x ATK & HP for Dragon & Devil Types; 3.5x ATK when Light & Dark attack at once');
 
 --Billy
-insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDescription) values
-('The Queen''s Lucky Star','ATK x2 when simultaneously clearing 4 connected Water orbs. ATK x0.5 for each additional orb, up to ATK x4 at 8 connected orb'),
-('Spirit of the Princess','ATK x2 when simultaneously clearing 4 connected Water orbs. ATK x0.5 for additional orb, up to ATK x5 at 10 connected orb'),
-('Eternal Dragon Pact - Fragrem','Light attribute cards HP x2, ATK x2.5. Light attribute cards ATK x1.5 on the turn a skill is used');
+insert into SWTS1103.dbo.LeaderSkill(LeaderSkillName, LeaderSkillDesc) values
+('The Queen''s Lucky Star','ATK increases when erasing 4+ linked Water Orbs, up to a maximum of 4x'),
+('Spirit of the Princess','ATK increases when erasing 4+ linked Water Orbs, up to 5x'),
+('Eternal Dragon Pact - Fragrem','2x HP & 2.5x ATK for Light Att.; 1.5x ATK for Light Att. when skills used');
