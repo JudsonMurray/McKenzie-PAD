594c89eee9bb03ea58bff80660d1b96bed9bdb93
DESKTOP-2AM8DD7\kyleg <kyle.gunton@Mckenzie.edu>
2017-04-21 11:23:39 -0300

    Updated Insert Order Kyles's inserts order fixed

--- INSERTS/EvolutionTree-Inserts.sql
+++ INSERTS/EvolutionTree-Inserts.sql
@@ -5,6 +5,7 @@
 --V 1.0 --Billy's EvoTree Added
 --V 1.0 --Kyle's EvoTree Added
 
+--Billy's
 INSERT INTO SWTS1103.dbo.EvolutionTree
 (NextMonsterID, BaseMonsterID, EvoMaterialOneID, EvoMaterialTwoID, EvoMaterialThreeID, EvoMaterialFourID, EvoMaterialFiveID, MinLevel, Devolveable, LevelReset)
 VALUES
@@ -15,16 +16,17 @@ VALUES
 (1733, 1068, 321, 1294, 1294, 247, 251, 1, 1, 0),
 (2658, 1733, 1324, 148, 1294, 1326, 1326, 1, 1, 0)
 
+--Kyle's
 INSERT INTO SWTS1103.dbo.EvolutionTree
 (NextMonsterID, BaseMonsterID, EvoMaterialOneID, EvoMaterialTwoID, EvoMaterialThreeID, EvoMaterialFourID, EvoMaterialFiveID, MinLevel, Devolveable, LevelReset)
 Values
 --Phantom God, Odin Tree
-(1107, 1108, 321, 171, 234, 246, 251, 50, 0,1), 
-(1108, 2179, 147, 321, 246, 250, 251, 1, 1,0),
-(1108, 3392, 147, 1085, 1085, 1085, 246, 1, 1, 0),
+(1108, 1107, 321, 171, 234, 246, 251, 50, 0,1), 
+(2179, 1108, 147, 321, 246, 250, 251, 1, 1,0),
+(3392, 1108, 147, 1085, 1085, 1085, 246, 1, 1, 0),
 --Castor Tree
-(2423, 2424, 151, 175, 250, 247, 251, 50, 0, 1),
-(2424, 3061, 151, 1176, 234, 250, 247, 1, 1, 0),
-(2424, 3062, 151, 1176, 234, 250, 249, 1, 1, 0)
+(2424,2423, 151, 175, 250, 247, 251, 50, 0, 1),
+(3061,2424, 151, 1176, 234, 250, 247, 1, 1, 0),
+(3062, 2424,151, 1176, 234, 250, 249, 1, 1, 0)
 
 select * from EvolutionTree
