98c89a51072716df7e43bd860e26f7f3687a45c3
Necrono <Cronoblast@Hotmail.com>
2017-04-24 13:52:52 -0300

    Attribute and Type Inserts

---
+++ INSERTS/AttributeAndType-Inserts.sql
@@ -0,0 +1,24 @@
+-- Attribute and type inserts --
+
+INSERT INTO SWTS1103.dbo.Attribute ("AttributeName") VALUES 
+('Fire'),
+('Water'),
+('Wood'),
+('Light'),
+('Dark');
+GO
+
+INSERT INTO SWTS1103.dbo.MonsterType (MonsterTypeName) VALUES 
+('God'),
+('Devil'),
+('Machine'),
+('Healer'),
+('Dragon'),
+('Physical'),
+('Attacker'),
+('Balanced'),
+('Evo Material'),
+('Enhance Material'),
+('Awaken Material'),
+('Redeemable Material');
+GO
