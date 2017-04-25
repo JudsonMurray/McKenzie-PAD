19963a2c87a3431f6fa50817a8fb3c4d0b4fe6ad
DESKTOP-0A6UE3H\elieg <elie.godbout@mckenzie.edu>
2017-04-25 11:25:36 -0300

    April 25 2017 - Fixed create all tables Changed Instance table to MonsterInstance

--- SCHEMA/Create-All-Tables.sql
+++ SCHEMA/Create-All-Tables.sql
@@ -2,8 +2,8 @@
 -- Created By Elie and billy.--
 
 --MonsterType Table
-if OBJECT_ID('SWTS1103.dbo.Instance', 'U') is not null
- drop table Instance;
+if OBJECT_ID('SWTS1103.dbo.MonsterInstance', 'U') is not null
+ drop table MonsterInstance;
 if OBJECT_ID('SWTS1103.dbo.Player', 'U') is not null
  drop table Player;
 if OBJECT_ID('SWTS1103.dbo.EvolutionTree', 'U') is not null
@@ -187,17 +187,17 @@ REFERENCES SWTS1103.dbo.Attribute( AttributeName ) ON DELETE NO ACTION ON UPDATE
 GO
 
 --Instance Table
-CREATE TABLE SWTS1103.dbo.Instance ( 
+CREATE TABLE SWTS1103.dbo.MonsterInstance ( 
 	InstanceID           int NOT NULL IDENTITY(100000,1),
 	PlayerID             int NOT NULL   ,
 	MonsterClassID        int NOT NULL   ,
 	PRIMARY KEY ( InstanceID )
  );
 
-ALTER TABLE SWTS1103.dbo.Instance 
-ADD CONSTRAINT FK_Instance_MonsterClassID FOREIGN KEY ( MonsterClassID ) 
+ALTER TABLE SWTS1103.dbo.MonsterInstance
+ADD CONSTRAINT FK_MonsterInstance_MonsterClassID FOREIGN KEY ( MonsterClassID ) 
 REFERENCES SWTS1103.dbo.MonsterClass( MonsterClassID ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE SWTS1103.dbo.Instance 
-ADD CONSTRAINT FK_Instance_PlayerID FOREIGN KEY ( PlayerID ) 
+ALTER TABLE SWTS1103.dbo.MonsterInstance 
+ADD CONSTRAINT FK_MonsterInstance_PlayerID FOREIGN KEY ( PlayerID ) 
 REFERENCES SWTS1103.dbo.Player( PlayerID ) ON DELETE NO ACTION ON UPDATE NO ACTION;
