c27717353b22d3e06f701407bdcdc25d28463a79
Necrono <Cronoblast@Hotmail.com>
2017-04-25 10:36:52 -0300

    Fixed my mistake, BaseMonsterId to MonsterClassID

--- SCHEMA/etc/CreateTableInstance.sql
+++ SCHEMA/etc/CreateTableInstance.sql
@@ -1,12 +1,12 @@
 CREATE TABLE SWTS1103.dbo.Instance ( 
 	InstanceID           int NOT NULL IDENTITY(100000,1),
 	PlayerID             int NOT NULL   ,
-	BaseMonsterID        int NOT NULL   ,
+	MonsterClassID        int NOT NULL   ,
 	PRIMARY KEY ( InstanceID )
  );
 
 ALTER TABLE SWTS1103.dbo.Instance 
-ADD CONSTRAINT FK_Instance_BaseMonsterID FOREIGN KEY ( BaseMonsterID ) 
+ADD CONSTRAINT FK_Instance_MonsterClassID FOREIGN KEY ( MonsterClassID ) 
 REFERENCES SWTS1103.dbo.MonsterClass( MonsterClassID ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE SWTS1103.dbo.Instance 
