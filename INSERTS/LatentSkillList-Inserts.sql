cb7b919f6017f2f8d84b8eae2867e1cf4eaf79ec
DESKTOP-2AM8DD7\kyleg <kyle.gunton@Mckenzie.edu>
2017-04-26 08:58:41 -0300

    Added LatentSkillList inserts
    Added LatentSkillList inserts for my monsters

---
+++ INSERTS/LatentSkillList-Inserts.sql
@@ -0,0 +1,11 @@
+--LatentSkillList Inserts
+
+--NO InstanceID's !!!
+--Kyle's
+Insert into swts1103.dbo.LatentSkillList (InstanceID, LatentSkillOne, LatentSkillTwo, LatentSkillThree, LatentSkillFour, LatentSkillFive, LatentSkillSix)
+( , 'Improved Healing', 'Improved HP', 'Dark Damage Reduction', 'Light Damage Reduction', 'Water Damage Reduction', NULL), --1730
+( , 'Improved Attack', 'Improved HP', 'Improved HP', 'Skill Delay Resist', 'Extend Move Time', NULL), --3305
+( , 'Improved Healing', 'Dark Damage Reduction', 'Light Damage Reduction', 'Extend Move Time', 'Extend Move Time', NULL), --2495
+( , 'Light Damage Reduction', 'Water Damage Reduction', 'Dark Damage Reduction', 'Wood Damage Reduction', 'Skill Delay Resist', NULL), --393
+( , 'Auto-Heal', 'Improved Attack', 'Improved HP', NULL, NULL, NULL) --3062
+
